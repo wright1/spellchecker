57e3bba494cffd6fc113051323f12868e64804b9
wright1 <prosperwright@yahoo.co.uk>
2018-12-02 06:21:11 +0000

    server created

---
+++ Src/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const router = require('./router');
+const port = process.env.port || 4040
+
+const server = http.createServer(router);
+
+server.listen(port, () => {
+    console.log(`Ther server is listening at port ${port}`)
+})
